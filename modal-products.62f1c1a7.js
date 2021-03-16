973a1833048416cd3bb4de902ce164e3d31575e9
Doroshuk <dorosukolga100@gmail.com>
2021-03-16 18:00:12 +0200

    Updates

--- modal-products.62f1c1a7.js
+++ modal-products.62f1c1a7.js
@@ -213,7 +213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62167" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58175" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
